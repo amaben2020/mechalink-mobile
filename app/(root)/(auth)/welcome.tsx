a78cb5465a16a0d8d1703f0727e1eeaa2cac6585
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-01-03 10:52:54 +0100

    font implementation

--- app/(root)/(auth)/welcome.tsx
+++ app/(root)/(auth)/welcome.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from 'react-native-swiper';
+import { onboarding } from '../../../constants/Icons';
 
 const Welcome = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState<number | undefined>(undefined);
   return (
-    <SafeAreaView>
-      <Text>Onboarding</Text>
+    <SafeAreaView className="flex h-full justify-between">
+      <Swiper
+        loop={false}
+        ref={swiperRef}
+        dot={
+          <View className="w-[32px] mx-1 bg-[#e2e8f0] h-[8px] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] mx-1 bg-primary-500 h-[8px] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex justify-center h-full p-8 gap-4">
+            <Image
+              source={item.image}
+              className="h-[300px] w-full"
+              resizeMode="contain"
+            />
+            <View>
+              <Text className="text-red-700 font-JakartaBold text-2xl">
+                {item.title}
+              </Text>
+
+              <Text className="text-red-700">{item.description}</Text>
+            </View>
+
+            <TouchableOpacity>
+              <Text>Next</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
--- constants/Icons.ts
+++ constants/Icons.ts
@@ -24,24 +24,24 @@ import selectedMarker from '@/assets/icons/selected-marker.png';
 import star from '@/assets/icons/star.png';
 import target from '@/assets/icons/target.png';
 import to from '@/assets/icons/to.png';
-// import check from '@/assets/images/check.png';
-// import getStarted from '@/assets/images/get-started.png';
-// import message from '@/assets/images/message.png';
-// import noResult from '@/assets/images/no-result.png';
-// import onboarding1 from '@/assets/images/onboarding1.png';
-// import onboarding2 from '@/assets/images/onboarding2.png';
-// import onboarding3 from '@/assets/images/onboarding3.png';
-// import signUpCar from '@/assets/images/signup-car.png';
+import check from '@/assets/images/check.png';
+import getStarted from '@/assets/images/get-started.png';
+import message from '@/assets/images/message.png';
+import noResult from '@/assets/images/no-result.png';
+import onboarding1 from '@/assets/images/onboarding1.png';
+import onboarding2 from '@/assets/images/onboarding2.png';
+import onboarding3 from '@/assets/images/onboarding3.png';
+import signUpCar from '@/assets/images/signup-car.png';
 
 export const images = {
-  // onboarding1,
-  // onboarding2,
-  // onboarding3,
-  // getStarted,
-  // signUpCar,
-  // check,
-  // noResult,
-  // message,
+  onboarding1,
+  onboarding2,
+  onboarding3,
+  getStarted,
+  signUpCar,
+  check,
+  noResult,
+  message,
 };
 
 export const icons = {
@@ -79,21 +79,21 @@ export const onboarding = [
     title: 'The perfect ride is just a tap away!',
     description:
       'Your journey begins with Ryde. Find your ideal ride effortlessly.',
-    // image: images.onboarding1,
+    image: images.onboarding1,
   },
   {
     id: 2,
     title: 'Best car in your hands with Ryde',
     description:
       'Discover the convenience of finding your perfect ride with Ryde',
-    // image: images.onboarding2,
+    image: images.onboarding2,
   },
   {
     id: 3,
     title: "Your ride, your way. Let's go!",
     description:
       'Enter your destination, sit back, and let us take care of the rest.',
-    // image: images.onboarding3,
+    image: images.onboarding3,
   },
 ];
 
